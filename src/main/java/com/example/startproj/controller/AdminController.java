6f19921137354f0a430310f6ecf95351f2bd33b6
Илья Есипов <esipov54321@gmail.com>
2025-05-03 20:54:41 +0500

    Ready task

---
+++ src/main/java/com/example/startproj/controller/AdminController.java
@@ -0,0 +1,21 @@
+package com.example.startproj.controller;
+
+import com.example.startproj.dto.UserDTO;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/api")
+public class AdminController {
+
+    @GetMapping
+    public UserDTO getAdminUser(@AuthenticationPrincipal UserDetails userDetails) {
+        return new UserDTO(
+                userDetails.getUsername(),
+                userDetails.getAuthorities().toString()
+        );
+    }
+}
